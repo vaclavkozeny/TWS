d5219c00f13e07cb7db881e9a5d11cb03b2b1faf
Václav Kožený <vaclav.kozeny@tul.cz>
2025-02-16 13:48:28 +0100

    update

--- sp/script.js
+++ sp/script.js
@@ -1,7 +1,18 @@
 let menuButton = document.getElementById('hamburger');
 let dropdownMenu = document.getElementById('dropdown');
+let imageContainers = document.querySelectorAll('figure');
 if(menuButton && dropdownMenu){
     menuButton.addEventListener('click',()=>{
         dropdownMenu.classList.toggle('hidden')
     })
+}
+if (imageContainers) {
+    imageContainers.forEach(e => {
+        e.addEventListener('click', () => {
+            let capt = e.querySelector('figcaption'); // Najdi figcaption
+            capt.classList.toggle('hidden'); // Přepni třídu
+            let img = e.querySelector('img');
+            img.classList.toggle('captionOn');
+        });
+    });
 }
